4fedb6b588698abd0caad073b10491887838f177
John McCabe <john@johnmccabe.net>
2017-01-15 20:52:30 +0000

    simplifying by creating functions

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -10,13 +10,17 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		StartGame ();
+	}
+
+	void StartGame () {
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
 
 
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
-	
+
 		print ("Is the numner higher or lower than " + guess + "?");
 		print ("Up = higher, down = lower, return = equal");
 
@@ -30,15 +34,19 @@ public class NumberWizard : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.UpArrow)) {
             //print("up arrow pressed");
 			min = guess;
-			guess = (max + min) / 2;
-			print ("Higher or lower than " + guess + "?");
+			NextGuess ();
 		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			//print("down arrow pressed");
 			max = guess;
-			guess = (max + min) / 2;
-			print ("Higher or lower than " + guess + "?");
+			NextGuess ();
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
             print("I won!");
 		} 
+	}
+
+	void NextGuess  () {
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
 	}
 }
